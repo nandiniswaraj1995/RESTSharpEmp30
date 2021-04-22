7a40139a1664d62f80e132fdb97474f608475a7b
unknown <nandiniswaraj13@gmail.com>
2021-04-22 15:43:11 +0530

    [Nandini]using RESTSharp  for api testin

---
+++ RESTSharpEmp30/RESTSharpEmp30/Employee.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RESTSharpTest
+{
+    class Employee
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string Salary
+        {
+            get;
+            set;
+
+        }
+    }
+}
